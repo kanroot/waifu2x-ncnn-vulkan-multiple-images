47126ebc44c2dec829cc9dfb2c9f231dc3c7b4a1
kan <root.kan.ss@gmail.com>
2022-01-12 15:39:25 -0300

    add prefix to output

--- GetImages.py
+++ GetImages.py
@@ -11,3 +11,10 @@ class GetImages:
         for filename in glob.iglob(self.folder + '**/*.' + self.format_to_find, recursive=True):
             self.list_images.append(filename)
         return self.list_images
+
+    def get_list_name_output(self, prefix):
+        list_output = []
+        for name_original in self.list_images:
+            last_slash = name_original.rfind("/") + 1
+            list_output.append(name_original[:last_slash] + prefix + name_original[last_slash:])
+        return list_output
--- main.py
+++ main.py
@@ -15,7 +15,4 @@ gpu_id = 'auto'
 if __name__ == '__main__':
     folder = GetImages(folder_images, format_to_find)
     list_images = folder.get_list_images()
-    print(list_images)
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    list_name_output = folder.get_list_name_output(prefix_output)
