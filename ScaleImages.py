f903e30bf8b92ed79c463b09a44dcbd1c8782660
kan <root.kan.ss@gmail.com>
2022-01-12 17:40:52 -0300

    Enable TTA

--- ScaleImages.py
+++ ScaleImages.py
@@ -6,11 +6,14 @@ class ScaleImages:
         self.list_input = list_names_to_transform
         self.list_output = list_names_output
 
-    def execute_commands(self, waifu2x, noise_level, scale):
+    def execute_commands(self, waifu2x, noise_level, scale, enable_tta):
         list_command = []
         for i in range(len(self.list_input)):
             command = waifu2x + ' -i' + ' ' + self.list_input[i] + ' -o ' + self.list_output[i]
             command += ' -n ' + str(noise_level) + ' -s ' + str(scale)
+            if enable_tta:
+                command += ' -x'
             list_command.append(command)
+
         for command in list_command:
             subprocess.call([str(command)], shell=True)
--- main.py
+++ main.py
@@ -3,22 +3,22 @@ from ScaleImages import ScaleImages
 
 folder_images = '/home/kan/Imágenes/MedievalArt/'
 format_to_find = 'png'
-#don't work with full path, use '~'
+# don't work with full path, use '~'
 folder_output = '~/Imágenes/'
 prefix_output = 'CUDA_'
 
 # parameters
 waifu2x = 'waifu2x-ncnn-vulkan'
-noise_level = 0
-scale_level = 2
-tile_size = 0
-model_path = 'models-cunet'
+noise_level = 0  # denoise level (-1/0/1/2/3, default=0)
+scale_level = 2  # upscale ratio (1/2/4/8/16/32, default=2)
+tile_size = 0  # tile size (>=32/0=auto, default=0) can be 0,0,0 for multi-gpu
+model_path = 'models-cunet'  # (default=models-cunet/models-upconv_7_anime_style_art_rgb)
 gpu_id = 'auto'
+enable_tta = False  # TTA mode able to reduce several type of artifacts but it's 8x slower than non TTA mode.
 
 if __name__ == '__main__':
     folder = GetImages(folder_images, format_to_find)
     list_images = folder.get_list_images()
     list_names_output = folder.get_list_name_output(prefix_output, folder_output)
     command = ScaleImages(list_images, list_names_output)
-    command.execute_commands(waifu2x, noise_level, scale_level)
-
+    command.execute_commands(waifu2x, noise_level, scale_level, enable_tta)
